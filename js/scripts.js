7fa1c5e7cb21248ed342c3ce8d29e8dc65036736
Connor Bell <shedding.sounds@gmail.com>
2021-07-12 16:12:59 -0400

    update with brandon's help

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 function hideBurger() {
     var x = 
-document.getElementById("burger")
+document.getElementById("menu")
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
 }
+onclick="hideBurger()"
