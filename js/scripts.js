0e7e686a9ce2b32a1a4b0a0260d20c734edc503b
Connor Bell <shedding.sounds@gmail.com>
2021-07-12 15:31:29 -0400

    add test script to hide burger menu

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function hideBurger() {
+    var x = 
+document.getElementById("burger")
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
