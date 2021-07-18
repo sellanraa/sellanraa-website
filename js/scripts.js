36a88bc189648f816a44a11bf758cec561a23b1f
Connor Bell <shedding.sounds@gmail.com>
2021-07-18 14:53:42 -0400

    fuck a burger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-function myFunction() {
-    var x = document.getElementById("other-pages");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  } 
+// function myFunction() {
+//     var x = document.getElementById("other-pages");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   } 
