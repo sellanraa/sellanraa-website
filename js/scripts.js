c179f9a646d5892abbe9d24a11c2fce1345ed4cd
Connor Bell <shedding.sounds@gmail.com>
2021-07-19 17:05:43 -0400

    fixed menu wrap issue and add random splash image loader

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
-// function myFunction() {
-//     var x = document.getElementById("other-pages");
-//     if (x.style.display === "block") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "block";
-//     }
-//   } 
+// Random 'who i am' photo loader
+
+var images = [
+    './img/me/me1.jpg',
+    './img/me/me2.jpg',
+    './img/me/me3.jpg',
+    './img/me/me4.jpg',
+    './img/me/me5.jpg',
+    './img/me/me6.jpg',
+    './img/me/me7.jpg',
+    './img/me/me8.jpg',
+    './img/me/me9.jpg',
+    './img/me/me10.jpg',
+    './img/me/me11.jpg',
+    './img/me/me12.jpg',
+    './img/me/me13.jpg'
+];
+
+var image = 
+    document.getElementById('random-image');
+
+image.src = images[Math.floor(Math.random() * Math.floor(images.length))]
+
+// Enlarge ween photo interface
+
